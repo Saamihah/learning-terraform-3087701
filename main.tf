331f661cf30fb0796d7feb9793c42c10c27597a7
Saamihah <saamihahadeshina@gmail.com>
2024-10-18 22:23:52 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target = {}
+      target = {
         my_target = {
       target_id        = aws_instance.blog.id
       port = 80
