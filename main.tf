d13c38690682eb08f56201f1ce4d813086c90a1c
Saamihah <saamihahadeshina@gmail.com>
2024-10-18 21:40:43 +0100

    create new dev vpc

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
+
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -34,7 +34,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "blog" { 
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
