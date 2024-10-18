7067c7b2c1d5a1fd7f5cb638cdb1c730258bebbf
Saamihah <saamihahadeshina@gmail.com>
2024-10-18 22:06:07 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -53,29 +53,7 @@ module "alb" {
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.protocol
   security_groups = module.blog_sg.security_group_id
-
-  
-
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
+}
 
   target_groups = [
     {
@@ -89,7 +67,6 @@ module "alb" {
 
   listeners =[
     {
-    ex-http-https-redirect = {
       port     = 80
       protocol = "HTTP"
       target_group_index = 0
