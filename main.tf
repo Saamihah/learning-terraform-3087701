dac0e40ac5a8ec9b19177471fe8d51f4422ca449
Saamihah <saamihahadeshina@gmail.com>
2024-10-18 21:07:15 +0100

    create new dev vpc

--- main.tf
+++ main.tf
@@ -27,8 +27,6 @@ module "blog_vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -38,8 +36,9 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [module.blog_new.security_group_id]
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
