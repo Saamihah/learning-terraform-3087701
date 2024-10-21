4bb227554428e71003709ccda587d266d37788f5
Saamihah <saamihahadeshina@gmail.com>
2024-10-21 13:30:33 +0100

    load bal

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ module "alb" {
       protocol = "HTTP"
       default_action = {
         type             = "forward"
-        target_group_key = "ex_instance"
+        target_group_arn = module.alb.target_group_arn[0]
       }
     }
   ]
