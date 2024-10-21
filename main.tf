11e05d4cc01ee9b6f019ddceac0d02b782704c6f
Saamihah <saamihahadeshina@gmail.com>
2024-10-21 13:49:41 +0100

    load b

--- main.tf
+++ main.tf
@@ -55,16 +55,6 @@ module "alb" {
 
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
-
   listeners = [
     {
       port     = 80
@@ -75,6 +65,16 @@ module "alb" {
       }
     }
   ]
+  
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
 
   tags = {
     Environment = "dev"
